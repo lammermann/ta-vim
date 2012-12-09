df64da36808583bc78023d65fba86876258a043c
Benjamin Kober <kober@optisense.com>
2012-12-09 23:01:03 +0100

    Editing is now only permitted in insert and in replace mode.

--- init.lua
+++ init.lua
@@ -2,6 +2,12 @@
 require 'textadept'
 editing = require 'textadept.editing'
 
+-- dummy function replace key insertion
+local function do_nothing()
+  return -- don't do anything. Just prevent other actions
+end
+
+-- the modes modul
 local M = {}
 
 --[[ This comment is for LuaDoc.
@@ -18,20 +24,23 @@ function M.use_vim_modes(keys)
   local modes = {}
   
   modes._keys = {}
-  modes._cur_mode = {}
+  modes._cur = {}
   
   for key,val in pairs(keys) do
     modes._keys[key] = val
   end
   
   function modes.switch(self, mode)
-    self._cur_mode = self[mode]
+    self._cur = self[mode]
   end
   
   function modes.__index(self, key)
-    val = modes._cur_mode[key]
+    val = modes._cur.keys[key]
     if val == nil then
       val = modes._keys[key]
+    end
+    if val == nil and modes._cur.ignore_defaults then
+      return do_nothing
     end
     return val
   end
@@ -50,85 +59,101 @@ function M.use_vim_modes(keys)
       --k = TODO
       --l = TODO
       --h = TODO
-    }
+  }
   keys['cv'] = nil -- Workaround for visual block mode
   
   -- Normal Mode
   modes['normal'] = {
-    -- other modes
-    i  = { modes.switch, modes, 'insert'},
-    R  = function()
-      modes:switch('replace')
-      buffer:edit_toggle_overtype()
-    end,
-    v  = { modes.switch, modes, 'visual'},
-    cv = { modes.switch, modes, 'visual_block'},
-    sv = { modes.switch, modes, 'visual_line'},
-    -- undo / redo
-    u  = buffer.undo,
-    cr = buffer.redo,
-    -- movement
-    g  = {
-      g = buffer.document_start,
-      },
-    G  = buffer.document_end,
-    j  = buffer.line_down,
-    k  = buffer.line_up,
-    l  = buffer.char_right,
-    h  = buffer.char_left,
-    w  = buffer.word_right,
-    b  = buffer.word_left,
-    e  = buffer.word_right_end,
-    ['$'] = buffer.line_end,
-    }
+    ignore_defaults = true,
+    keys = {
+      -- other modes
+      i  = { modes.switch, modes, 'insert'},
+      R  = function()
+        modes:switch('replace')
+        buffer:edit_toggle_overtype()
+      end,
+      v  = { modes.switch, modes, 'visual'},
+      cv = { modes.switch, modes, 'visual_block'},
+      sv = { modes.switch, modes, 'visual_line'},
+      -- undo / redo
+      u  = buffer.undo,
+      cr = buffer.redo,
+      -- movement
+      g  = {
+        g = buffer.document_start,
+        },
+      G  = buffer.document_end,
+      j  = buffer.line_down,
+      k  = buffer.line_up,
+      l  = buffer.char_right,
+      h  = buffer.char_left,
+      w  = buffer.word_right,
+      b  = buffer.word_left,
+      e  = buffer.word_right_end,
+      ['$'] = buffer.line_end,
+      }
+  }
   
   -- Insert Mode
   modes['insert'] = {
-    ['esc'] = { modes.switch, modes, 'normal'}
+    keys = {
+      ['esc'] = { modes.switch, modes, 'normal'}
     }
+  }
   
   -- Replace Mode
   modes['replace'] = {
-    ['esc']  = function()
-      modes:switch('normal')
-      buffer:edit_toggle_overtype()
+    keys = {
+      ['esc']  = function()
+        modes:switch('normal')
+        buffer:edit_toggle_overtype()
     end,
     }
+  }
   
   -- Visual Mode
   modes['visual'] = {
-    ['esc'] = { modes.switch, modes, 'normal'},
-    g  = {
-      g = buffer.document_start_extend,
-      },
-    G  = buffer.document_end_extend,
-    j  = buffer.line_down_extend,
-    k  = buffer.line_up_extend,
-    l  = buffer.char_right_extend,
-    h  = buffer.char_left_extend,
-    w  = buffer.word_right_extend,
-    b  = buffer.word_left_extend,
-    e  = buffer.word_right_end_extend,
-    ['$'] = buffer.line_end_extend,
+    ignore_defaults = true,
+    keys = {
+      ['esc'] = { modes.switch, modes, 'normal'},
+      g  = {
+        g = buffer.document_start_extend,
+        },
+      G  = buffer.document_end_extend,
+      j  = buffer.line_down_extend,
+      k  = buffer.line_up_extend,
+      l  = buffer.char_right_extend,
+      h  = buffer.char_left_extend,
+      w  = buffer.word_right_extend,
+      b  = buffer.word_left_extend,
+      e  = buffer.word_right_end_extend,
+      ['$'] = buffer.line_end_extend,
     }
+  }
   
   -- Visual Block Mode
   modes['visual_block'] = {
-    ['esc'] = { modes.switch, modes, 'normal'},
-    j  = buffer.line_down_rect_extend,
-    k  = buffer.line_up_rect_extend,
-    l  = buffer.char_right_rect_extend,
-    h  = buffer.char_left_rect_extend,
-    w  = buffer.word_right_rect_extend,
-    b  = buffer.word_left_rect_extend,
-    e  = buffer.word_right_end_rect_extend,
-    ['$'] = buffer.line_end_rect_extend,
+    ignore_defaults = true,
+    keys = {
+      ['esc'] = { modes.switch, modes, 'normal'},
+      j  = buffer.line_down_rect_extend,
+      k  = buffer.line_up_rect_extend,
+      l  = buffer.char_right_rect_extend,
+      h  = buffer.char_left_rect_extend,
+      w  = buffer.word_right_rect_extend,
+      b  = buffer.word_left_rect_extend,
+      e  = buffer.word_right_end_rect_extend,
+      ['$'] = buffer.line_end_rect_extend,
     }
+  }
   
   -- Visual Line Mode
   modes['visual_line'] = {
-    ['esc'] = { modes.switch, modes, 'normal'},
+    ignore_defaults = true,
+    keys = {
+      ['esc'] = { modes.switch, modes, 'normal'},
     }
+  }
   
   -- initialize modul
   modes:switch('normal')
