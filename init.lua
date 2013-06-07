3d513bf676d90ed0ea005516aa2a9b950fe0039c
Benjamin Kober <kober@optisense.com>
2013-06-08 00:20:35 +0200

    updated to textadept version 6.6

--- init.lua
+++ init.lua
@@ -10,6 +10,11 @@ end
 -- the modes modul
 local M = {}
 
+function M.mode_switch(mode)
+  _G.keys.MODE = mode
+  events.emit("VIM_SWITCH_MODE", mode)
+end
+
 --[[ This comment is for LuaDoc.
 ---
 -- Defines modes for keybindings like in vim.
@@ -21,35 +26,53 @@ local M = {}
 --]]
 
 function M.use_vim_modes(keys)
-  local modes = {}
 
-  modes._keys = {}
-  modes._cur = {}
-
-  for key,val in pairs(keys) do
-    modes._keys[key] = val
-  end
-
-  function modes.switch(self, mode)
-    self._cur = self[mode]
-    buffer.caret_style =
-      self._cur.caret_style or _SCINTILLA.constants.CARETSTYLE_LINE
-  end
-
-  function modes.__index(self, key)
-    val = modes._cur.keys[key]
-    if val == nil then
-      val = modes._keys[key]
+  events.connect(events.UPDATE_UI, function()
+    if keys.MODE == 'normal' then
+      buffer.caret_style = _SCINTILLA.constants.CARETSTYLE_BLOCK
+    elseif keys.MODE == 'replace' then
+      gui.statusbar_text = '-- REPLACE --'
+      buffer.caret_style = _SCINTILLA.constants.CARETSTYLE_LINE
+    elseif keys.MODE == 'visual' then
+      gui.statusbar_text = '-- VISUAL --'
+      buffer.caret_style = _SCINTILLA.constants.CARETSTYLE_LINE
+    elseif keys.MODE == 'visual_block' then
+      gui.statusbar_text = '-- VISUAL BLOCK --'
+      buffer.caret_style = _SCINTILLA.constants.CARETSTYLE_LINE
+    elseif keys.MODE == 'visual_line' then
+      gui.statusbar_text = '-- VISUAL LINE --'
+      buffer.caret_style = _SCINTILLA.constants.CARETSTYLE_LINE
+    else
+      gui.statusbar_text = '-- INSERT --'
+      buffer.caret_style = _SCINTILLA.constants.CARETSTYLE_LINE
     end
-    if val == nil and modes._cur.ignore_defaults then
-      return do_nothing
-    end
-    return val
-  end
+  end)
 
-  M.multiply = ''
+  events.connect("VIM_SWITCH_MODE", function()
+    if keys.MODE == 'normal' then
+      gui.statusbar_text = ''
+      buffer.caret_style = _SCINTILLA.constants.CARETSTYLE_BLOCK
+    elseif keys.MODE == 'replace' then
+      gui.statusbar_text = '-- REPLACE --'
+      buffer.caret_style = _SCINTILLA.constants.CARETSTYLE_LINE
+    elseif keys.MODE == 'visual' then
+      gui.statusbar_text = '-- VISUAL --'
+      buffer.caret_style = _SCINTILLA.constants.CARETSTYLE_LINE
+    elseif keys.MODE == 'visual_block' then
+      gui.statusbar_text = '-- VISUAL BLOCK --'
+      buffer.caret_style = _SCINTILLA.constants.CARETSTYLE_LINE
+    elseif keys.MODE == 'visual_line' then
+      gui.statusbar_text = '-- VISUAL LINE --'
+      buffer.caret_style = _SCINTILLA.constants.CARETSTYLE_LINE
+    else
+      gui.statusbar_text = '-- INSERT --'
+      buffer.caret_style = _SCINTILLA.constants.CARETSTYLE_LINE
+    end
+  end)
 
   -- {{{ helper functions
+
+  M.multiply = ''
 
   -- perform `action` for `M.multiply` times
   local function multiply_action(action, ...)
@@ -66,16 +89,18 @@ function M.use_vim_modes(keys)
   end
 
   local function add_multiply_bindings(mode)
-    modes[mode].keys['esc'] = function() M.multiply='' end
+    keys[mode]['esc'] = function() M.multiply='' end
     for i = 0, 9 do
       number = tostring(i)
-      modes[mode].keys[number] = {append_multiply_buffer, number}
+      keys[mode][number] = {append_multiply_buffer, number}
     end
   end
 
   -- }}}
 
   -- Global Keys
+  keys['esc'] = function() keys.MODE = 'normal' end
+
   -- window commands
   keys['cw'] = {
       s = { view.split, view },
@@ -93,153 +118,161 @@ function M.use_vim_modes(keys)
   keys['cv'] = nil -- Workaround for visual block mode
 
   -- Normal Mode
-  modes['normal'] = {
-    ignore_defaults = true,
-    caret_style = _SCINTILLA.constants.CARETSTYLE_BLOCK,
-    keys = {
-      -- other modes
-      i  = { modes.switch, modes, 'insert'},
-      I  = function()
-        buffer.home()
-        modes:switch('insert')
-      end,
-      O  = function()
-        buffer.line_up()
-        buffer.line_end()
-        buffer.new_line()
-        modes:switch('insert')
-      end,
-      o  = function()
-        buffer.line_end()
-        buffer.new_line()
-        modes:switch('insert')
-      end,
-      A  = function()
-        buffer.line_end()
-        modes:switch('insert')
-      end,
-      R  = function()
-        modes:switch('replace')
-        buffer:edit_toggle_overtype()
-      end,
-      v  = { modes.switch, modes, 'visual'},
-      cv = { modes.switch, modes, 'visual_block'},
-      sv = { modes.switch, modes, 'visual_line'},
-      [':'] = gui.command_entry.focus,
-      -- undo / redo
-      u  = buffer.undo,
-      cr = buffer.redo,
-      -- movement
-      g  = {
-        g = buffer.document_start,
-        },
-      G  = buffer.document_end,
-      j  = {multiply_action, buffer.line_down},
-      k  = {multiply_action, buffer.line_up},
-      l  = {multiply_action, buffer.char_right},
-      h  = {multiply_action, buffer.char_left},
-      w  = {multiply_action, buffer.word_right},
-      b  = {multiply_action, buffer.word_left},
-      e  = {multiply_action, buffer.word_right_end},
-      ['$'] = buffer.line_end,
-      --0 = buffer.home,
-      -- cut, copy, paste
-      d  = {
-        d = buffer.line_cut,
-        l = function()
-          buffer:hide_selection(true)
-          buffer.char_right_extend()
-          buffer.cut()
-          buffer:hide_selection(false)
-        end,
-        h = function()
-          buffer:hide_selection(true)
-          buffer.char_left_extend()
-          buffer.cut()
-          buffer:hide_selection(false)
-        end,
+  keys.normal = {
+    -- other modes
+    i  = M.mode_switch,
+    I  = function()
+      buffer.home()
+      M.mode_switch()
+    end,
+    O  = function()
+      buffer.line_up()
+      buffer.line_end()
+      buffer.new_line()
+      M.mode_switch()
+    end,
+    o  = function()
+      buffer.line_end()
+      buffer.new_line()
+      M.mode_switch()
+    end,
+    a  = function()
+      buffer:char_right()
+      M.mode_switch()
+    end,
+    A  = function()
+      buffer.line_end()
+      M.mode_switch()
+    end,
+    R  = function()
+      buffer:edit_toggle_overtype()
+      M.mode_switch("replace")
+    end,
+    v  = { M.mode_switch, "visual" },
+    cv = { M.mode_switch, "visual_block" },
+    V  = { M.mode_switch, "visual_line" },
+    [':'] = gui.command_entry.focus,
+    -- undo / redo
+    u  = buffer.undo,
+    cr = buffer.redo,
+    -- movement
+    g  = {
+      g = buffer.document_start,
       },
+    G  = function()
+      if M.multiply == '' then
+        buffer:document_end()
+      else
+        buffer:goto_line(tonumber(M.multiply) - 1)
+        M.multiply = ''
+      end
+    end,
+    j  = {multiply_action, buffer.line_down},
+    k  = {multiply_action, buffer.line_up},
+    l  = {multiply_action, buffer.char_right},
+    h  = {multiply_action, buffer.char_left},
+    w  = {multiply_action, buffer.word_right},
+    b  = {multiply_action, buffer.word_left},
+    e  = {multiply_action, buffer.word_right_end},
+    ['$'] = buffer.line_end,
+    --0 = buffer.home,
+    -- cut, copy, paste
+    d  = {
+      d = buffer.line_cut,
+      l = function()
+        buffer:hide_selection(true)
+        buffer.char_right_extend()
+        buffer.cut()
+        buffer:hide_selection(false)
+      end,
+      h = function()
+        buffer:hide_selection(true)
+        buffer.char_left_extend()
+        buffer.cut()
+        buffer:hide_selection(false)
+      end,
+      j = function()
+        buffer:line_cut()
+        buffer:line_cut()
+      end,
+      k = function()
+        buffer:line_up()
+        buffer:line_cut()
+        buffer:line_cut()
+      end,
+    },
+    -- folds
+    z = {
+      c = function()
+        local line, num = buffer.get_cur_line()
+        buffer:fold_children(num, _SCINTILLA.constants.SC_FOLDACTION_CONTRACT)
+      end,
+      o = function()
+        local line, num = buffer.get_cur_line()
+        buffer:fold_children(num, _SCINTILLA.constants.SC_FOLDACTION_EXPAND)
+      end,
       }
-  }
+    }
   add_multiply_bindings("normal")
 
-  -- Insert Mode
-  modes['insert'] = {
-    keys = {
-      ['esc'] = { modes.switch, modes, 'normal'}
-    }
-  }
-
-  -- Replace Mode
-  modes['replace'] = {
-    keys = {
-      ['esc']  = function()
-        modes:switch('normal')
-        buffer:edit_toggle_overtype()
-    end,
-    }
-  }
-
   -- Visual Mode
-  modes['visual'] = {
-    ignore_defaults = true,
-    keys = {
-      ['esc'] = { modes.switch, modes, 'normal'},
-      g  = {
-        g = buffer.document_start_extend,
-        },
-      G  = buffer.document_end_extend,
-      j  = {multiply_action, buffer.line_down_extend},
-      k  = {multiply_action, buffer.line_up_extend},
-      l  = {multiply_action, buffer.char_right_extend},
-      h  = {multiply_action, buffer.char_left_extend},
-      w  = {multiply_action, buffer.word_right_extend},
-      b  = {multiply_action, buffer.word_left_extend},
-      e  = {multiply_action, buffer.word_right_end_extend},
-      ['$'] = buffer.line_end_extend,
-      -- cut
-      d = function()
-        buffer.cut()
-        modes:switch('normal')
-      end,
-    }
+  keys.visual = {
+    ['esc'] = { M.mode_switch, "normal" },
+    g  = {
+      g = buffer.document_start_extend,
+      },
+    G  = buffer.document_end_extend,
+    j  = {multiply_action, buffer.line_down_extend},
+    k  = {multiply_action, buffer.line_up_extend},
+    l  = {multiply_action, buffer.char_right_extend},
+    h  = {multiply_action, buffer.char_left_extend},
+    w  = {multiply_action, buffer.word_right_extend},
+    b  = {multiply_action, buffer.word_left_extend},
+    e  = {multiply_action, buffer.word_right_end_extend},
+    ['$'] = buffer.line_end_extend,
+    -- cut
+    d = function()
+      buffer.cut()
+      M.mode_switch("normal")
+    end,
   }
 
   -- Visual Block Mode
-  modes['visual_block'] = {
-    ignore_defaults = true,
-    keys = {
-      ['esc'] = { modes.switch, modes, 'normal'},
-      j  = {multiply_action, buffer.line_down_rect_extend},
-      k  = {multiply_action, buffer.line_up_rect_extend},
-      l  = {multiply_action, buffer.char_right_rect_extend},
-      h  = {multiply_action, buffer.char_left_rect_extend},
-      w  = {multiply_action, buffer.word_right_rect_extend},
-      b  = {multiply_action, buffer.word_left_rect_extend},
-      e  = {multiply_action, buffer.word_right_end_rect_extend},
-      ['$'] = buffer.line_end_rect_extend,
-      -- cut
-      d = function()
-        buffer.cut()
-        modes:switch('normal')
-      end,
-    }
+  keys.visual_block = {
+    ['esc'] = { M.mode_switch, "normal" },
+    j  = {multiply_action, buffer.line_down_rect_extend},
+    k  = {multiply_action, buffer.line_up_rect_extend},
+    l  = {multiply_action, buffer.char_right_rect_extend},
+    h  = {multiply_action, buffer.char_left_rect_extend},
+    w  = {multiply_action, buffer.word_right_rect_extend},
+    b  = {multiply_action, buffer.word_left_rect_extend},
+    e  = {multiply_action, buffer.word_right_end_rect_extend},
+    ['$'] = buffer.line_end_rect_extend,
+    -- cut
+    d = function()
+      buffer.cut()
+      M.mode_switch("normal")
+    end,
   }
 
   -- Visual Line Mode
-  modes['visual_line'] = {
-    ignore_defaults = true,
-    keys = {
-      ['esc'] = { modes.switch, modes, 'normal'},
-    }
+  keys.visual_line = {
+    ['esc'] = { M.mode_switch, "normal" },
+  }
+
+  -- Replace Mode
+  keys.replace = {
+    ['esc']  = function()
+      M.mode_switch("normal")
+      buffer:edit_toggle_overtype()
+    end,
   }
 
   -- initialize modul
-  modes:switch('normal')
-  setmetatable(keys, modes)
-  M._modes = modes
+  M.mode_switch('normal') -- default mode
 
   return keys
+
 end
 
 function M.bind_key(mode, key, fun, remove_old)
